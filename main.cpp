bdc42276e46d13d4fa559294693448de9d88f517
DrinoSan <s.becirbegovic@gmail.com>
2023-12-25 11:32:51 +0000

    DELETING INFORMATION FOR LAYER STUFF

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+#include <cstring>
 #include <iostream>
+#include <netinet/if_ether.h>
 #include <pcap.h>
+#include <thread>
+#include <libnet.h>
 
 // ArpChat includes
 #include "messages.h"
@@ -40,178 +44,46 @@ void print_packet( const unsigned char* packet, int length )
 void packet_handler( unsigned char* user, const struct pcap_pkthdr* pkthdr,
                      const unsigned char* packet )
 {
+    print_packet( packet, pkthdr->len );
+    std::cout << "------------------------------------" << std::endl;
     if ( !ArpChat::EthernetFrame::isArp( packet ) )
     {
         // We are only interested in arp packages
         return;
     }
 
-    ArpChat::EthernetFrame frame(packet, pkthdr->caplen);
+    // PLEN protocol address length which is in ipv4 = 4 because ip address is
+    // 4 byte long We will use this to set our length of our message Also PLEN
+    // sets the limit of our messages which is 1 byte or 255 characters
+
+    // Our chat will use as a prefix identifier sand
+    // And then followed by our message
+
+    // SPA will be our message we want to send
+
+    ArpChat::EthernetFrame frame( packet, pkthdr->caplen );
     std::cout << "Destination Mac: " << frame.destinationMacAddr << '\n';
     std::cout << "Source Mac:      " << frame.sourceMacAddr << '\n';
 
-    // const unsigned char* ethernet_header = packet;
+    std::cout << "ARP Frame Information:\n";
+    std::cout << "Hardware Type: 0x" << std::hex << frame.payload.htype
+              << std::dec << "\n";
+    std::cout << "Ether type / Frame Type: 0x" << std::hex << frame.etherType
+              << std::dec << "\n";
+    std::cout << "Hardware Size: " << static_cast<int>( frame.payload.hlen )
+              << "\n";
+    std::cout << "Protocol Size: " << static_cast<int>( frame.payload.plen )
+              << "\n";
+    std::cout << "Operation Code: " << frame.payload.oper << "\n";
 
-    // // Print the whole frame
-    // print_packet( packet, pkthdr->caplen );
+    std::cout << "Sender MAC Address: " << frame.payload.sha;
+    // print_mac_address( arp_packet + SENDER_HARDWARE_ADDRESS );
+    std::cout << "\n";
 
-    // // Important: The physicall layer is alredy unpacking the frame form layer
-    // // 1, basically preamble and sfd is already cut off when we receive it here
-    // // Assuming EtherType is in big-endian format -> Networks use big-endian
-    // uint16_t ether_type =
-    //     ( ethernet_header[ 12 ] << 8 ) | ethernet_header[ 13 ];
-
-    // // ARP EtherType
-    // if ( ether_type == 0x0806 )
-    // {
-    //     const unsigned char* arp_packet =
-    //         packet + 14;   // ARP packet starts at offset 14
-    //                        // 6 Byte Source Mac address
-    //                        // 6 Byte Destination Mac address
-    //                        // 2 Byte EtherType
-
-    //     // ARP header fields
-    //     // https://en.wikipedia.org/wiki/Address_Resolution_Protocol#cite_note-IANA-2
-    //     uint16_t hardware_type = ( arp_packet[ 0 ] << 8 ) | arp_packet[ 1 ];
-    //     uint16_t protocol_type =
-    //         ( arp_packet[ 2 ] << 8 ) |
-    //         arp_packet[ 3 ];   // This is mostly ipv4 0x0800.
-    //                            // It identifies the network layer protocol for
-    //                            // which arp is resolving the address
-    //     uint8_t hardware_size =
-    //         arp_packet[ 4 ];   // Length of hardware length (Mac address)
-    //     uint8_t protocol_size =
-    //         arp_packet[ 5 ];   // Length of protocol network address ipv4 = 4
-    //                            // byte example: 255.255.255.255
-    //     uint16_t operation_code =
-    //         ( arp_packet[ 6 ] << 8 ) |
-    //         arp_packet[ 7 ];   // Specifies the operation that the sender is
-    //                            // performing: 1 for request, 2 for reply.
-
-        std::cout << "ARP Frame Information:\n";
-        std::cout << "Hardware Type: 0x" << std::hex << frame.payload.htype
-                  << std::dec << "\n";
-        std::cout << "Ether type / Frame Type: 0x" << std::hex << frame.etherType
-                  << std::dec << "\n";
-        std::cout << "Hardware Size: " << static_cast<int>( frame.payload.hlen )
-                  << "\n";
-        std::cout << "Protocol Size: " << static_cast<int>( frame.payload.plen )
-                  << "\n";
-        std::cout << "Operation Code: " << frame.payload.oper << "\n";
-
-        std::cout << "Sender MAC Address: " << frame.payload.sha;
-        // print_mac_address( arp_packet + SENDER_HARDWARE_ADDRESS );
-        std::cout << "\n";
-
-        // std::cout << "Sender IP Address: ";
-        // for ( int i = 0; i < 4; ++i )
-        // {
-        //     std::cout << static_cast<int>(
-        //         arp_packet[ SENDER_PROTOCOL_ADDRESS + i ] );
-        //     if ( i < 3 )
-        //         std::cout << ".";
-        // }
-        // std::cout << "\n";
-
-        // std::cout << "Target MAC Address: ";
-        // print_mac_address( arp_packet + TARGET_HARDWARE_ADDRESS );
-        // std::cout << "\n";
-
-        // std::cout << "Target IP Address: ";
-        // for ( int i = 0; i < 4; ++i )
-        // {
-        //     std::cout << static_cast<int>(
-        //         arp_packet[ TARGET_PROTOCOL_ADDRESS + i ] );
-        //     if ( i < 3 )
-        //         std::cout << ".";
-        // }
-        std::cout << "\n";
-    // }
+    std::cout << "\n";
 }
 
-// void packet_handler( unsigned char* user, const struct pcap_pkthdr* pkthdr,
-//                      const unsigned char* packet )
-// {
-//     const unsigned char* ethernet_header = packet;
-
-//     // Print the whole frame
-//     print_packet( packet, pkthdr->caplen );
-
-//     // Important: The physicall layer is alredy unpacking the frame form layer
-//     // 1, basically preamble and sfd is already cut off when we receive it here
-//     // Assuming EtherType is in big-endian format -> Networks use big-endian
-//     uint16_t ether_type =
-//     ( ethernet_header[ 12 ] << 8 ) | ethernet_header[ 13 ];
-
-//     // ARP EtherType
-//     if ( ether_type == 0x0806 )
-//     {
-//         const unsigned char* arp_packet =
-//     packet + 14;   // ARP packet starts at offset 14
-//     // 6 Byte Source Mac address
-//     // 6 Byte Destination Mac address
-//     // 2 Byte EtherType
-
-//     // ARP header fields
-//     // https://en.wikipedia.org/wiki/Address_Resolution_Protocol#cite_note-IANA-2
-//     uint16_t hardware_type = ( arp_packet[ 0 ] << 8 ) | arp_packet[ 1 ];
-//     uint16_t protocol_type =
-//     ( arp_packet[ 2 ] << 8 ) |
-//     arp_packet[ 3 ];   // This is mostly ipv4 0x0800.
-//     // It identifies the network layer protocol for
-//     // which arp is resolving the address
-//     uint8_t hardware_size =
-//     arp_packet[ 4 ];   // Length of hardware length (Mac address)
-//     uint8_t protocol_size =
-//     arp_packet[ 5 ];   // Length of protocol network address ipv4 = 4
-//     // byte example: 255.255.255.255
-//     uint16_t operation_code =
-//     ( arp_packet[ 6 ] << 8 ) |
-//     arp_packet[ 7 ];   // Specifies the operation that the sender is
-//     // performing: 1 for request, 2 for reply.
-
-//         std::cout << "ARP Frame Information:\n";
-//         std::cout << "Hardware Type: 0x" << std::hex << hardware_type
-//                   << std::dec << "\n";
-//         std::cout << "Ether type / Frame Type: 0x" << std::hex << protocol_type
-//                   << std::dec << "\n";
-//         std::cout << "Hardware Size: " << static_cast<int>( hardware_size )
-//                   << "\n";
-//         std::cout << "Protocol Size: " << static_cast<int>( protocol_size )
-//                   << "\n";
-//         std::cout << "Operation Code: " << operation_code << "\n";
-
-//         std::cout << "Sender MAC Address: ";
-//         print_mac_address( arp_packet + SENDER_HARDWARE_ADDRESS );
-//         std::cout << "\n";
-
-//         std::cout << "Sender IP Address: ";
-//         for ( int i = 0; i < 4; ++i )
-//         {
-//             std::cout << static_cast<int>(
-//         arp_packet[ SENDER_PROTOCOL_ADDRESS + i ] );
-//         if ( i < 3 )
-//         std::cout << ".";
-//         }
-//         std::cout << "\n";
-
-//         std::cout << "Target MAC Address: ";
-//         print_mac_address( arp_packet + TARGET_HARDWARE_ADDRESS );
-//         std::cout << "\n";
-
-//         std::cout << "Target IP Address: ";
-//         for ( int i = 0; i < 4; ++i )
-//         {
-//             std::cout << static_cast<int>(
-//         arp_packet[ TARGET_PROTOCOL_ADDRESS + i ] );
-//         if ( i < 3 )
-//         std::cout << ".";
-//         }
-//         std::cout << "\n";
-//     }
-// }
-
-int main()
+void capturePackets()
 {
     char errbuf[ PCAP_ERRBUF_SIZE ];
 
@@ -221,7 +93,7 @@ int main()
     if ( handle == nullptr )
     {
         std::cerr << "Could not open device: " << errbuf << std::endl;
-        return -1;
+        return;
     }
 
     // Set a filter to capture only ARP packets
@@ -242,19 +114,97 @@ int main()
     {
         std::cerr << "Couldn't parse filter " << filter_exp << ": "
                   << pcap_geterr( handle ) << std::endl;
-        return -1;
+        return;
     }
 
     if ( pcap_setfilter( handle, &fp ) == -1 )
     {
         std::cerr << "Couldn't install filter " << filter_exp << ": "
                   << pcap_geterr( handle ) << std::endl;
-        return -1;
+        return;
     }
 
     // Start capturing packets and pass them to the packet_handler function
     pcap_loop( handle, 0, packet_handler, nullptr );
 
     pcap_close( handle );
+}
+
+void sendGratuitousArp() {
+    libnet_t *l;
+    char errbuf[LIBNET_ERRBUF_SIZE];
+
+    // Initialize the library
+    l = libnet_init(LIBNET_LINK, "en0", errbuf);
+    if (l == nullptr) {
+        std::cerr << "libnet_init() failed: " << errbuf << std::endl;
+        return;
+    }
+
+    // Replace these with your actual MAC and IP addresses
+    uint8_t sourceMac[] = {0x74, 0x8f, 0x3c, 0xb9, 0x8f, 0xf5};
+    uint8_t sourceIp[] = {192, 168, 0, 99};
+    uint8_t broadcastMac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+    std::string customData = "HI THIS IS A TEST";
+    // Build ARP packet
+    libnet_ptag_t arp_tag = libnet_build_arp(
+        ARPHRD_ETHER,
+        ETHERTYPE_IP,
+        6,
+        customData.size(),
+        ARPOP_REQUEST,
+        sourceMac,
+        reinterpret_cast<uint8_t*>(customData.data()),
+        broadcastMac,
+        reinterpret_cast<uint8_t*>(customData.data()),
+        nullptr, 0,
+        l, 0
+    );
+
+    if (arp_tag == -1) {
+        std::cerr << "libnet_build_arp() failed: " << libnet_geterror(l) << std::endl;
+        libnet_destroy(l);
+        return;
+    }
+
+    // Build Ethernet frame
+    libnet_ptag_t eth_tag = libnet_build_ethernet(
+        broadcastMac,
+        sourceMac,
+        ETHERTYPE_ARP,
+        nullptr, 0,
+        l, 0
+    );
+
+    if (eth_tag == -1) {
+        std::cerr << "libnet_build_ethernet() failed: " << libnet_geterror(l) << std::endl;
+        libnet_destroy(l);
+        return;
+    }
+
+    // Write the packet to the network
+    int bytes_written = libnet_write(l);
+    if (bytes_written == -1) {
+        std::cerr << "libnet_write() failed: " << libnet_geterror(l) << std::endl;
+    } else {
+        std::cout << "Gratuitous ARP packet sent successfully." << std::endl;
+    }
+
+    // Cleanup
+    libnet_destroy(l);
+}
+
+int main()
+{
+    // Start a thread for packet capture
+    std::thread captureThread( capturePackets );
+
+    // Sending packet
+    sendGratuitousArp();
+
+    // Wait for the capture thread to finish
+    captureThread.join();
+
     return 0;
 }
